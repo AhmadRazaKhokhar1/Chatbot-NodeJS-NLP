1ca91b12455f23e48e1eb30af7f3d221395e8057
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-02-12 12:59:22 +0500

    data set is added and frontend is also connected

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express =  require('express');
+const cors = require('cors');
 const chatBotRouter = require('./routes/chatBotRoute.js');
 const port = 3001;
 
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 //using the route
 app.use('/api/v1', chatBotRouter);
 
